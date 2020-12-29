1b3ddc4903e35c98d13ce3e162559b9bf0d93d42
David Bauer <davbauer@ucdavis.edu>
2020-12-29 11:17:49 +0100

    Updated README and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <!-- This file will be used to layout and connect your views -->
 <template>
   <div id="app">
+      <!-- This is only a basic setup to display your views. Use HTML and CSS to create your layout using these basic commands -->
       <View1 />
       <View2 />
   </div>
